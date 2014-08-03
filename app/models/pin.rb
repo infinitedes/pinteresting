0725e4527f896baede8d4be8be8ae49baa784834
Des Kennedy <des@infinitedesign.com>
2014-08-03 20:31:09 +0100

    changed pin.rb to solve add pin on live app

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,5 +2,10 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+:storage => :s3,
+            :bucket => '******',
+            :s3_credentials => {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
+
+
 end
 
