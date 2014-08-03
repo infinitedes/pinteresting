73a3909dcd9c162a577d15e48bddad40f55969c4
Des Kennedy <des@infinitedesign.com>
2014-08-03 20:39:21 +0100

    updated name of s3 bucket

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,7 +3,7 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 :storage => :s3,
-            :bucket => '******',
+            :bucket => 'pinfinitemedia',
             :s3_credentials => {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
 
 
