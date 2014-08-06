403a9692329f5d64a7cf02d5f5a419e25d6a6417
Des Kennedy <des@infinitedesign.com>
2014-08-06 22:24:27 +0100

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,5 +3,9 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
+
+validates :description, presence: true
+validates :image, presence: true
+
 end
 
