64cfb8c3897ebe1ba1df746ce7f12ad53a16830c
Des Kennedy <des@infinitedesign.com>
2014-08-12 21:05:57 +0100

    changed S3 access in pins.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,13 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
+	 :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+}
 
 
 validates :description, presence: true
